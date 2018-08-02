c0fa6c2876647c0f48a51affc11c1ff0bce8cb8b
Frank McCallion <fkmccallion@gmail.com>
2018-08-02 03:51:37 +0000

    automatically backed up by learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,7 @@
 class TweetsController < ApplicationController
 
   get '/tweet/:id' do
-    @tweet = Tweet.find_by_id(params[:id])
+    @tweet = Tweet.find(params[:id])
     erb :'tweets/show'
   end
 
